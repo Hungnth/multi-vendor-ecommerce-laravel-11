99c6fee1f700032023832b431179980728170500
Hungnth <thienhungnth@gmail.com>
2024-12-13 13:55:45 +0700

    Add Flash Sale Function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Frontend\FlashSaleController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\Frontend\UserProfileController;
@@ -19,10 +20,12 @@ require __DIR__ . '/auth.php';
 
 Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
 
+Route::get('flash-sale', [FlashSaleController::class, 'index'])->name('flash-sale');
+
 // User route
-Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.'], function () {
-    Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
-});
+// Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.'], function () {
+//     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+// });
 
 // User route
 Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user', 'as' => 'user.'], function () {
